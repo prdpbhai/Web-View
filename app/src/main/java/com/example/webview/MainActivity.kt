91dadf37a18780930dbf3e299cc44f012c1056a5
winklixllc <pradeepyadavhacker@gmail.com>
2024-03-08 12:50:43 +0530

    final commite

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true // Enable JavaScript for YouTube Player
+        webView.loadUrl("https://www.youtube.com/embed/1wECsnGZcfc") // Load YouTube live video URL
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+    }
+}
